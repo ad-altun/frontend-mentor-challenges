595c0621c8b490b687bd12fd32bc569e887f029a
ad-altun <adaltun@web.de>
2025-06-13 20:46:07 +0200

    Filter dropdown content changes between 'Filter by Region' and 'All'.

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,18 +1,28 @@
-// import { homePage } from "../service/api";
+import { useState } from "react"
 
 interface FilterProps {
     onSelect: (term: string) => void
 }
 
 export default function Filter({ onSelect }: FilterProps) {
+    const [selected, setSelected] = useState<boolean>(true);
 
     // use 'Set' store unique values of Regions
     // const regions: string[] = [...new Set(homePage.map((item) => item.region!))];
 
     const filterChangeHandler = (region: string) => {
         onSelect(region)
+        setSelected((prev) => {
+            return !prev
+        })
         // const selectedRegion = homePage.filter((country) => country.region === region)
         // return selectedRegion
+    }
+
+    let content = "Filter by Region"
+
+    if (!selected) {
+        content = "All"
     }
 
     return (
@@ -20,7 +30,7 @@ export default function Filter({ onSelect }: FilterProps) {
             <div   >
                 <select name="" id="select-filter" className="select-filter"
                     onChange={e => { filterChangeHandler(e.target.value) }}>
-                    <option value="All">Filter by Region</option>
+                    <option value="All">{content}</option>
                     <option value="Africa">Africa</option>
                     <option value="Americas">Americas</option>
                     <option value="Asia">Asia</option>
